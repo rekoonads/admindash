05f952e6fcb338cead62fa47229274170315cce9
KOODOS Admin <admin@koodos.in>
2025-08-17 18:49:30 +0530

    Fix Prisma type error

--- lib/prisma-production.ts
+++ lib/prisma-production.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: any
 }
 
 export const prisma = globalForPrisma.prisma ?? 
@@ -10,4 +10,4 @@ export const prisma = globalForPrisma.prisma ??
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   }).$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma as any
